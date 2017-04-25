2adf73b32f39ca8aaf22a99706cfb93f1f810e2c
ZzlosS <strahinja.stojadinovic@pmf.edu.rs>
2017-04-25 21:09:07 +0200

    t3

---
+++ languages/lang.en.php
@@ -0,0 +1,39 @@
+<?php
+	
+	$lang = array();
+	$lang['Guest'] = 'Log in or Sign up';
+	$lang['Welcome'] = 'Welcome to ';
+	$lang['Home'] = 'Home';
+	$lang['Login'] = 'Log in';
+	$lang['Signup'] = 'Sign up';
+	$lang['NotLogged'] = 'You must be logged in to view this site.';
+	$lang['UserTaken'] = ' This email address is already in use';
+	$lang['UserAvailable'] = ' This username is available';
+	$lang['Email'] = 'Email address';
+	$lang['Pass'] = 'Password';
+	$lang['Question'] = 'Do you even lift bro?'; //nesto normalnije
+	$lang['NotAll'] = 'Not all fields were entered';
+	$lang['AccCreated'] = '<h4>Account created</h4>Please log in';
+	$lang['Wrong'] = 'Email address is not valid.';
+	$lang['NotInUse'] = 'There is no account with that email address';
+	$lang['PassInvalid'] = 'Password is not correct';
+	$lang['LoggedIn'] = 'You are now logged in. Please <a href="profile.php?view=$id">click here</a> to continue.<br>';
+	$lang['loggedOut'] = '<div class="main">You have been logged out. Please <a href="index.php">click here</a> to refresh the screen.';
+	$lang['CantLogOut'] = '<div class="main">You cannont log out because you are not logged in.';
+	$lang['YP'] = '<h3>Your Profile</h3>';
+	$lang['YPP'] = 'Your profile picture';
+	$lang['EditP'] = 'Enter or edit your details';
+	$lang['FName'] = 'First name';
+	$lang['LName'] = 'Last name';
+	$lang['Image'] = 'Image';
+	$lang['SP'] = 'Save Profile';
+	$lang['CP'] = 'Change Profile';
+	$lang['Members'] = 'Members';
+	$lang['Galery'] = 'Galery';
+	$lang['Friends'] = 'Friends';
+	$lang['Messages'] = 'Messages';
+	$lang['Profile'] = 'Profile';
+	$lang['Logout'] = 'Log out';
+	$lang['PInformation'] = 'Profile information';
+			
+?>
---
+++ languages/lang.sr.php
@@ -0,0 +1,18 @@
+<?php
+
+	$lang = array();
+	$lang['Guest'] = 'Gost';
+	$lang['Welcome'] = 'Dobro dosli na ';
+	$lang['Home'] = 'Pocetna';
+	$lang['Login'] = 'Uloguj se';
+	$lang['Signup'] = 'Prijavi se';
+	$lang['NotLogged'] = 'Morate biti ulogovani da bi ste videli ovaj sajt.';
+	$lang['UserTaken'] = ' Ova email adresa je vec u upotrebi';
+	$lang['UserAvailable'] = ' Korisnicko ime je dostupno';
+	$lang['Email'] = 'Email adresa';
+	$lang['Pass'] = 'Sifra';
+	$lang['NotAll'] = 'Nisu popunjena sva polja';
+	$lang['AccCreated'] = '<h4>Nalog je napravljen</h4>Molimo vas da se ulogujete';
+	$lang['Wrong'] = 'Uneta email adresa nije validna';
+
+?>
