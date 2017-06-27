c1261406dd8c4699b5a4ace0ce2e2383fd9d49bd
JovanJocko <jovan.radenkovic@pmf.edu.rs>
2017-06-27 19:56:41 +0200

    28

--- home.php
+++ home.php
@@ -1,16 +1,39 @@
 <?php
 require_once 'basic.php';
-if(!$loggedIn) die();
+if(!$loggedIn) die();//da se skloni za guest
 
-    $result = qM("SELECT m.id,p.* FROM `members` AS `m` LEFT JOIN `pictures` as `p` ON `m`.`id`=`p`.`user_id` ORDER BY `pic_date` DESC LIMIT 0,2");
+    $result = qM("SELECT m.id,m.name,p.user_id,p.date_update,p.pic_path,p.pic_desc FROM `pictures` AS `p` LEFT JOIN `members` as `m` ON `p`.`user_id`=`m`.`id` ORDER BY `date_update` DESC LIMIT 0,2");
    /* $count = qM("SELECT * FROM `members`");
     $n = $count->num_rows;*/
 ?>
+    <style>
+        div.im {
+            margin: 5px 5px 20px 5px;
+            border: 1px solid #ccc;
+            width: 500px;
+            height: 570px;
+        }
+
+        div.im:hover {
+            border: 1px solid #777;
+        }
+
+        div.descN {
+            padding-bottom: 10px;
+            text-align: center;
+        }
+        div.desc {
+            padding-top: 10px;
+            text-align: center;
+        }
+    </style>
     <br><br>
-        <div class="images">
+        <div class="images" align="center">
             <?php
             while($row = $result->fetch_assoc()){
-                echo "<p align='center'><img src='".$row['pic_path']."' height='500' width='500'></p>".$row['date_update'];
+                echo "<div class='im'><div class='descN'><a href='members.php?id=".$row['id']."'>".$row['name']."</a></div>";
+                echo "<a target=_blank href='".$row['pic_path']."'><img src='".$row['pic_path']."' height='500' width='500'></a>";
+                echo "<div class='desc'>".$row['pic_desc']."</div></div>";
             }
             ?>
         </div>
