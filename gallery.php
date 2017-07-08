c44ea86acf57fa76477bc81e2ff7fe2fb55bbc9c
ZzlosS <strahinja.stojadinovic@pmf.edu.rs>
2017-07-08 14:26:44 +0200

    zzloss2.3

--- gallery.php
+++ gallery.php
@@ -74,63 +74,68 @@ if(!$loggedIn) die("<script>location.replace('home.php')</script>");
         $desc = sS($_POST['desc']);
     }
 
-    if(isset($_FILES['image']['name'])){
-        $name = date("YmdHis");
-        $saveto = "images/$id/album/".$name.".jpg";
-        move_uploaded_file($_FILES['image']['tmp_name'], $saveto);
-        qM("INSERT INTO `pictures`(`user_id`, `date_update`, `pic_path` `pic_desc`) 
+    if(isset($_FILES['image']['name'])) {
+        echo $_FILES['image']['size'];
+        if ($_FILES['image']['size'] > 2 * 1024 * 1024) {
+            echo "Greska";
+            //die("<script>alert('Slika je prevelika.')</script>");
+        } else {
+            $name = date("YmdHis");
+            $saveto = "images/$id/album/" . $name . ".jpg";
+            move_uploaded_file($_FILES['image']['tmp_name'], $saveto);
+            qM("INSERT INTO `pictures`(`user_id`, `date_update`, `pic_path`, `pic_desc`) 
                               VALUES('$id', '$date', '$saveto', '$desc')");
-        qM("INSERT INTO `log`(`date`, `msg`) VALUES('$date', '$gname($id) added picture $desc')");
-        $typeok = TRUE;
-        switch ($_FILES['image']['type']){
-            case "image/gif":
-                $src = imagecreatefromgif($saveto);
-                break;
-            case "image/jpeg": case "image/pjpeg":
-            $src = imagecreatefromjpeg($saveto);
-            break;
-            case "image/png":
-                $src = imagecreatefrompng($saveto);
-                break;
-            default:
-                $typeok = FALSE;
-                break;
-        }
-        if($typeok){
-            list($w, $h) = getimagesize($saveto);
-            $max = 500;
-            $tw = $w;
-            $th = $h;
-            $x1 = $x2 = 0;
-            if($w > $h && $w > $max){
-                $tw = $max;
-                $th = $h * $max / $w;
-                $x1 = $max/2 - $tw/2;
-                $x2 = $max/2 - $th/2;
+            qM("INSERT INTO `log`(`date`, `msg`) VALUES('$date', '$gname($id) added picture $desc')");
+            $typeok = TRUE;
+            switch ($_FILES['image']['type']) {
+                case "image/gif":
+                    $src = imagecreatefromgif($saveto);
+                    break;
+                case "image/jpeg":
+                case "image/pjpeg":
+                    $src = imagecreatefromjpeg($saveto);
+                    break;
+                case "image/png":
+                    $src = imagecreatefrompng($saveto);
+                    break;
+                default:
+                    $typeok = FALSE;
+                    break;
             }
-            elseif($h > $w && $h > $max){
-                $th = $max;
-                $tw = $max / $h * $w;
-                $x1 = $max/2 - $tw/2;
-                $x2 = $max/2 - $th/2;
-            }
-            elseif($w > $max){
-                $tw = $th = $max;
-                $x1 = $max/2 - $tw/2;
-                $x2 = $max/2 - $th/2;
-            }
-            $x1 = $max/2 - $tw/2;
-            $x2 = $max/2 - $th/2;
-            $tmp = imagecreatetruecolor($max, $max);
-            imagesavealpha($tmp, true);
-            $color = imagecolorallocatealpha($tmp, 0, 0, 0, 127);
-            imagefill($tmp, 0, 0, $color);
+            if ($typeok) {
+                list($w, $h) = getimagesize($saveto);
+                $max = 500;
+                $tw = $w;
+                $th = $h;
+                $x1 = $x2 = 0;
+                if ($w > $h && $w > $max) {
+                    $tw = $max;
+                    $th = $h * $max / $w;
+                    $x1 = $max / 2 - $tw / 2;
+                    $x2 = $max / 2 - $th / 2;
+                } elseif ($h > $w && $h > $max) {
+                    $th = $max;
+                    $tw = $max / $h * $w;
+                    $x1 = $max / 2 - $tw / 2;
+                    $x2 = $max / 2 - $th / 2;
+                } elseif ($w > $max) {
+                    $tw = $th = $max;
+                    $x1 = $max / 2 - $tw / 2;
+                    $x2 = $max / 2 - $th / 2;
+                }
+                $x1 = $max / 2 - $tw / 2;
+                $x2 = $max / 2 - $th / 2;
+                $tmp = imagecreatetruecolor($max, $max);
+                imagesavealpha($tmp, true);
+                $color = imagecolorallocatealpha($tmp, 0, 0, 0, 127);
+                imagefill($tmp, 0, 0, $color);
 
-            imagecopyresampled($tmp, $src, $x1, $x2, 0, 0, $tw, $th, $w, $h);
+                imagecopyresampled($tmp, $src, $x1, $x2, 0, 0, $tw, $th, $w, $h);
 
-            imagepng($tmp, $saveto);
-            imagedestroy($tmp);
-            imagedestroy($src);
+                imagepng($tmp, $saveto);
+                imagedestroy($tmp);
+                imagedestroy($src);
+            }
         }
     }
     if(isset($_GET['id'])){
